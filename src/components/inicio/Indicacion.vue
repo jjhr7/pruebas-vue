6710eac53580b2466856d516e9d90b0f19cb3cdd
mariaurma <lolitaurgiles0806@gmail.com>
2023-05-16 14:24:48 -0500

    Fase uno del index

---
+++ src/components/inicio/Indicacion.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="landing-titles">
+        <h1 style="text-align: justify;">
+            Control de jornadas. ¡Optimiza el tiempo y ahorra
+            dinero!
+        </h1>
+        <p style="text-align: justify;">
+            Controla a que hora entran y salen tus trabajadores.
+            Asignales tareas, gestiona el tiempo que están
+            trabajando y registra su productividad. ¡Aumenta la
+            eficiencia de tu equipo con solo una aplicación!
+        </p>
+        <a href="#" class="call-to-action">Pruebalo gratis</a>
+
+
+
+    </div>
+      <!-- Usado por -->
+      <section class="section-used">
+          <h2><span></span> Equipos que están mejorando su productividad</h2>
+          <div class="used-by">
+              <img src="../../assets/images/logo-upv.svg" alt="Logo de la Universidad Politécnica de Valencia">
+              <img id="logo-htsk" src="../../assets/images/htsk.svg" alt="Logo de HTSK">
+              <img id="logo-oink" src="../../assets/images/oink.svg" alt="Logo de Oink!">
+              <img src="../../assets/images/logo-ideas.svg" alt="Logo de Ideas UPV">
+          </div>
+      </section>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Indicacion",
+  data() {
+    return {};
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style>
+
+</style>
