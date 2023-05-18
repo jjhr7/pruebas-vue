7d7e156346d7677f237a027b9c0921d139ca1f8d
jeancar20 <jeancar20@hotmail.com>
2023-05-18 21:52:21 +0200

    resto de rutas

---
+++ src/pages/Equipo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="rui-page-title b-none">
+      <div class="container-fluid">
+          <h1 class="section-title">Equipo</h1>
+      </div>
+    </div>
+    <div class="rui-page-content">
+      <div class="container-fluid">
+
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: "Equipo",
+  data() {
+    return {};
+  },
+  methods: {
+
+  },
+};
+</script>
